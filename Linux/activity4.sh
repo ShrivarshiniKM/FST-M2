73d88767e00d7b5865ee2a04e62ba3bc3ffe1eee
ShrivarshiniKM <shrikm97@gmail.com>
2024-12-19 09:13:32 +0530

    Add files via upload

---
+++ Linux/activity4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# create .mp3 files
+
+touch song{1,2,3,4,5,6}.mp3
+touch song{1,2,3,4,5,6}.jpg
+
+touch snap{1,2,3,4,5,6}.jpg
+touch film{1,2,3,4,5,6}.mp4
+
+mkdir -p Music
+mkdir -p Pictures
+mkdir -p Videos
+
+
+mv *.mp3 Music/
+mv *.jpg Pictures/
+mv *.mp4 Videos/
+
+
